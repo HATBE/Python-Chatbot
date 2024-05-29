bdfe01173bbfae70122c08968360461ee0dd1123
HATBE2113 <admin@aarongensetter.ch>
2024-05-29 23:35:55 +0200

    gui finish

--- frontend/script.js
+++ frontend/script.js
@@ -1,37 +1,49 @@
-const ws = new WebSocket('ws://localhost:2000');
+const ws = new WebSocket("ws://localhost:2000");
 
 const messageFormEl = document.getElementById("message-form");
 const messageTextEl =  document.getElementById("message-text");
 const messageWrapper = document.getElementById("message-wrapper");
 
 ws.onopen = () => {
-    console.log('Connected to the server');
+    console.log("Connected to the server");
 };
 
 ws.onmessage = (event) => {
-    addMessage(event.data, 'left')
+    addMessage(event.data, "left");
+};
+
+ws.onclose = () => {
+    console.log("Connection closed");
+    addMessage("Connection Closed", "left");
+    messageTextEl.disabled = true;
+};
+
+
+ws.onerror = (error) => {
+    console.error("WebSocket error:", error);
+    addMessage("WebSocket error occurred", "left");
+    messageTextEl.disabled = true;
 };
 
 function addMessage(message, bubbleClass) {
-    const messageEl = document.createElement('div');
-    messageEl.classList.add('message', bubbleClass)
-    const messageInnerEl = document.createElement('div');
+    const messageEl = document.createElement("div");
+    messageEl.classList.add("message", bubbleClass)
+    const messageInnerEl = document.createElement("div");
     messageInnerEl.textContent = message;
     messageEl.appendChild(messageInnerEl);
     messageWrapper.appendChild(messageEl);
     messageWrapper.scrollTop = messageWrapper.scrollHeight;
 }
 
-
-messageFormEl.addEventListener('submit', (event) => {
+messageFormEl.addEventListener("submit", (event) => {
     event.preventDefault();
 
     const message = messageTextEl.value;
 
     if (message) {
-        addMessage(message, 'right')
+        addMessage(message, "right");
 
         ws.send(message);
-        messageTextEl.value = '';
+        messageTextEl.value = "";
     }
 });
